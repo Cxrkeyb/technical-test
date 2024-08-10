4168a3a8dd2bd9b116ba3bef21ee9008900e8c6f
Cxrkeyb <88465069+Cxrkeyb@users.noreply.github.com>
2024-08-10 10:14:30 -0500

    <Feat> Add router and swagger docs

--- backend/src/web/modules/v1/router.ts
+++ backend/src/web/modules/v1/router.ts
@@ -1,5 +1,10 @@
 import express from "express";
+// clientRouter
+import clientRouter from "@/web/modules/v1/client/router";
 
 const router = express.Router();
+
+// client
+router.use("/clientes", clientRouter);
 
 export default router;
